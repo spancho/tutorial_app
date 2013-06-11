6cdc8865e5f46050a39ac97d3ab6a59814eb95ac
Spencer Eldred <eldredspencer@gmail.com>
2013-06-10 19:49:39 -1000

    Remove 'Home' from home page title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,15 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :title => 'Sample App')
     end
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                  :text => "#{base_title} | Home")
+                  :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                                :text => " | Home")
     end
   end
 
